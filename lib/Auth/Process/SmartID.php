4e98678e257b24f0f6b4cb0a9b357bdfb90dec1b
Tim van Dijen <tim.dijen@minbzk.nl>
2019-05-27 13:59:11 +0200

    Address Psalm issues

--- lib/Auth/Process/SmartID.php
+++ lib/Auth/Process/SmartID.php
@@ -45,13 +45,6 @@ class SmartID extends \SimpleSAML\Auth\ProcessingFilter
      */
     private $add_candidate = true;
 
-    /**
-     * Attributes which should be added/appended.
-     *
-     * @var array Associative array of arrays.
-     */
-    private $attributes = [];
-
 
     /**
      * @param array $config
@@ -138,7 +131,7 @@ class SmartID extends \SimpleSAML\Auth\ProcessingFilter
 
         $id = $this->addID($request['Attributes'], $request);
 
-        if (isset($id)) {
+        if (!empty($id)) {
             $request['Attributes'][$this->id_attribute] = [$id];
         }
     }
